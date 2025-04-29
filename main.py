1841548cde2f05efdd7370aa639b2e1f9586ca4e
josha91130 <josha91130@gmail.com>
2025-04-29 02:01:21 +0000

    Add 2_Model_Visualization page

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+# --- main.py ---
+# 📚 Start Here: App Introduction
 import streamlit as st
 
-# Hide Main Page，jump to intro 
-st.switch_page("pages/0_Intro_to_App.py")
+st.set_page_config(page_title="Start Here", layout="wide")
+
+st.title("📚 Welcome to Trading Volume Prediction App")
+
+st.write("""
+Welcome!  
+This dashboard predicts SPY trading volumes based on market sentiment and macroeconomic indicators.
+Contributor: Zhe Yu Lin, Pei Chi Chu, Ming Hua Tsai
+👉 Please start by selecting **Intro to the App** on the sidebar.
+""")
