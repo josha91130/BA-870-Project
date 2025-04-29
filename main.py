4faca4c29d40543f4dd493cc17589e562763949a
josha91130 <josha91130@gmail.com>
2025-04-29 05:45:03 +0000

    Add 2_Model_Visualization page

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 import streamlit as st
 
-# Hide Main Page，jump to intro
-st.switch_page("pages/0_Intro_to_App.py")
+# --- Website setting ---
+st.set_page_config(page_title="Trading Volume App", layout="wide")
+
+# --- Sidebar Navigation ---
+with st.sidebar:
+    st.header("📚 Start Here")
+    st.page_link("pages/0_Intro_to_App.py", label="Intro to the App")
+    st.page_link("pages/1_Info.py", label="Info")
+
+    st.markdown("---")
+    st.header("📊 Dashboard Options")
+    st.page_link("pages/2_Model_Visualization.py", label="1) Model Visualization")
+    st.page_link("pages/3_Get_Features.py", label="2) Get Features")
+    st.page_link("pages/4_Predict_Volume.py", label="3) Predict Volume")
+
+# --- Main Page ---
+st.title("📚 Welcome to Trading Volume Prediction App")
+
+st.write("""
+Welcome!
+This dashboard predicts SPY trading volumes based on market sentiment and macroeconomic indicators.
+
+**Contributor:** Zhe Yu Lin, Pei Chi Chu, Ming Hua Tsai
+
+👉 Please start by selecting **Intro to the App** on the sidebar.
+""")
